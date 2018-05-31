4e936b808257fa61f7d0a3f1ec1d230f3276e8bb
Parthasarathi Yelampalle <parthasarathi.yelampalle@kony.com>
2018-05-31 10:47:27 +0530

    [Parthu] Adding FFI Related Files.

--- resources/customlibs/jsXml/Tealium.js
+++
@@ -1,4 +0,0 @@
-var Tealium={};
-Tealium.initSharedInstance= function(accountName,profileName,environmentName,key){};
-Tealium.trackEvent= function(callType,data,key){};
-Tealium.trackView= function(screenName,data,key){};
