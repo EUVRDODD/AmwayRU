4f846b0b494e5c5258810d4cbe1f2addcff38b4b
Parthasarathi Yelampalle <parthasarathi.yelampalle@kony.com>
2018-06-07 13:23:38 +0530

    [Parthu]Added FFI Related Files.

---
+++ resources/customlibs/jsSrc/winmobile/Tealium.js
@@ -0,0 +1,4 @@
+var Tealium={};
+Tealium.initSharedInstance= function(accountName,profileName,environmentName,key){};
+Tealium.trackEvent= function(callType,data,key){};
+Tealium.trackView= function(screenName,data,key){};
